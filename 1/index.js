3f9532be0d01a5cd6888f5896bde229f21bf56ed
Matthieu Bessol <matthieubessol@gmail.com>
2022-12-01 14:19:02 +0100

    day 1

---
+++ 1/index.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("./data.txt", "utf-8");
+
+const dataSplitted = data.split("\n");
+let caloriesByElf = [];
+let elfIndex = 0;
+dataSplitted.forEach((calorie) => {
+  if (!caloriesByElf[elfIndex]) {
+    caloriesByElf[elfIndex] = [];
+  }
+  if (calorie) {
+    caloriesByElf[elfIndex].push(parseInt(calorie, 10));
+  } else {
+    elfIndex++;
+  }
+});
+
+const dataByElf = caloriesByElf.map((el, index) => ({
+  index,
+  items: el,
+  total: el.reduce((a, b) => a + b),
+}));
+
+const getMaxCalories = (d) =>
+  d.map(({ total }) => total).sort((a, b) => b - a)?.[0];
+
+const maxCalories = getMaxCalories(dataByElf);
+
+console.log(maxCalories);
+
+// 2
+const getTopThreeMaxCalories = (d) =>
+  d
+    .map(({ total }) => total)
+    .sort((a, b) => b - a)
+    ?.slice(0, 3)
+    .reduce((a, b) => a + b);
+const topThrees = getTopThreeMaxCalories(dataByElf);
+
+console.log(topThrees);
