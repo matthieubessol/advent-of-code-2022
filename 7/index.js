3f95a03ffd8be52f7303179aac11ffde755d2fe2
Matthieu Bessol <matthieubessol@gmail.com>
2022-12-07 14:10:29 +0100

    day 7

---
+++ 7/index.js
@@ -0,0 +1,134 @@
+const fs = require("fs");
+
+const rows = fs
+  .readFileSync("./data.txt", "utf-8")
+  .split("\n")
+  .filter((a) => a);
+
+const isCommand = (row) => row.includes("$");
+
+let tree = [
+  {
+    name: "/",
+    type: "dir",
+    files: [],
+    directories: [],
+    parent: null,
+    size: 0,
+  },
+];
+let currentPath = [];
+
+rows.forEach((row) => {
+  const rowInfo = row.split(" ");
+
+  const isChangeDirectory = rowInfo[0] === "$" && rowInfo[1] === "cd";
+
+  if (isChangeDirectory && rowInfo[2].includes("..")) {
+    currentPath.pop();
+  } else if (isChangeDirectory) {
+    currentPath.push(rowInfo[2]);
+  }
+
+  const currentPathName = currentPath[currentPath.length - 1];
+  let currentTree = tree[0];
+  currentPath.slice(1, currentPath.length).forEach((p) => {
+    currentTree = currentTree.directories.find((t) => t.name === p);
+  });
+
+  // Result of ls
+  if (!isChangeDirectory) {
+    const fileSize = parseInt(rowInfo[0], 10);
+    const isDir = rowInfo[0].includes("dir");
+    if (isDir) {
+      currentTree.directories.push({
+        name: rowInfo[1],
+        type: "dir",
+        files: [],
+        directories: [],
+        parent: currentPathName,
+        size: 0,
+      });
+    }
+
+    if (fileSize && !isDir) {
+      currentTree.files.push({
+        name: rowInfo[1],
+        type: "file",
+        size: fileSize,
+        parent: currentPathName,
+      });
+    }
+  }
+});
+
+// const getFolderSize = (currentNode, size) => {
+//   if (currentNode.totalSize) return size;
+//   if (Array.isArray(currentNode)) {
+//     size += currentNode.map((a) => getFolderSize(a, size));
+//   } else {
+//     console.log(currentNode.files.map((f) => f.size));
+//     const currentFileSize = currentNode.files.map((f) => f.size).reduce((a, b) => a + b);
+//     size += currentFileSize
+//     currentNode.totalSize = getFolderSize(currentNode, 0);
+//   }
+
+//   return size;
+// };
+
+const maxSize = 100000;
+let nodesToDelete = [];
+
+const getFolderSize = (node) => {
+  let currentFileSize = 0;
+  const currentFileSizes = node.files.map(({ size }) => size);
+  if (currentFileSizes?.length) {
+    currentFileSize = currentFileSizes.reduce((a, b) => a + b);
+  }
+
+  const subFoldersSizes = node.directories.map((dir) => getFolderSize(dir));
+  let subFoldersSize = 0;
+  if (subFoldersSizes?.length) {
+    subFoldersSize = subFoldersSizes.reduce((a, b) => a + b);
+  }
+
+  const currentTotal = currentFileSize + subFoldersSize;
+  node.totalSize = currentTotal;
+
+  if (node.totalSize < maxSize) {
+    nodesToDelete.push(node);
+  }
+
+  return currentTotal;
+};
+
+getFolderSize(tree[0]);
+
+const totalDeleted = nodesToDelete
+  .map(({ totalSize }) => totalSize)
+  .reduce((a, b) => a + b);
+console.log({ totalDeleted });
+
+// 2
+
+const freeSpace = 70000000 - 43956976;
+const needed = 30000000 - freeSpace;
+
+let nodeToFreeSpace = [];
+const getFolderToFreeSpace = (node) => {
+  node.directories.map((dir) => getFolderToFreeSpace(dir));
+
+  if (node.totalSize >= needed) {
+    nodeToFreeSpace.push(node);
+  }
+
+  return node;
+};
+
+getFolderToFreeSpace(tree[0]);
+
+const folterToDelete = nodeToFreeSpace
+  .sort((a, b) => a.totalSize - b.totalSize)
+  .map((a) => a.totalSize)[0];
+
+console.log({ folterToDelete });
