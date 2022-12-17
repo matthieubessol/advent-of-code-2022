eb4eb4aa13d20542723390839ec321fa47cfd8de
Matthieu Bessol <matthieubessol@gmail.com>
2022-12-17 15:52:41 +0100

    day 11 - 1

---
+++ 11/index.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+const monkeys = fs
+  .readFileSync("./data.txt", "utf-8")
+  .trimEnd()
+  .split("Monkey")
+  .map((monkeyRows) =>
+    monkeyRows
+      .split("\n")
+      .slice(1, monkeyRows?.length)
+      .map((row) => row.trim())
+      .filter((a) => a)
+  )
+  .filter((a) => a?.length)
+  .map((monkeyInfo, key) => {
+    const testInfo = monkeyInfo[2].split(": ")[1].split(" by ");
+    const operationInfo = monkeyInfo[1].split(" = ")[1].split(" ");
+
+    return {
+      id: key,
+      items: monkeyInfo[0]
+        .split(": ")[1]
+        .split(", ")
+        .map((a) => parseInt(a, 10)),
+      mathOperation: {
+        first: parseInt(operationInfo[0], 10) || operationInfo[0],
+        operation: operationInfo[1],
+        second: parseInt(operationInfo[2], 10) || operationInfo[2],
+      },
+      test: {
+        operation: testInfo[0],
+        value: parseInt(testInfo[1], 10),
+      },
+      successMonkey: parseInt(monkeyInfo[3].split("monkey ")[1], 10),
+      failMonkey: parseInt(monkeyInfo[4].split("monkey ")[1], 10),
+      numberOfInspections: 0,
+    };
+  });
+
+const nbRound = 20;
+
+for (let i = 0; i < nbRound; i++) {
+  monkeys.forEach((monkey) => {
+    monkey.items.forEach((item, index) => {
+      // Get new value
+      let newItemValue = 0;
+      let first =
+        typeof monkey.mathOperation.first === "number"
+          ? monkey.mathOperation.first
+          : item;
+      let second =
+        typeof monkey.mathOperation.second === "number"
+          ? monkey.mathOperation.second
+          : item;
+      switch (monkey.mathOperation.operation) {
+        case "+":
+          newItemValue = first + second;
+          break;
+        case "*":
+          newItemValue = first * second;
+        default:
+          break;
+      }
+      newItemValue = parseInt(newItemValue / 3, 10);
+
+      // Check test
+      let success = false;
+      if (monkey.test.operation === "divisible") {
+        success = newItemValue % monkey.test.value === 0;
+      }
+
+      monkeys[success ? monkey.successMonkey : monkey.failMonkey].items.push(
+        newItemValue
+      );
+      monkey.numberOfInspections += 1;
+    });
+    monkey.items = [];
+  });
+}
+
+console.log(monkeys);
+
+const monkeyBusinessScore = monkeys
+  .map((a) => a.numberOfInspections)
+  .sort((a, b) => b - a)
+  .slice(0, 2)
+  .reduce((a, b) => a * b);
+
+console.log(monkeyBusinessScore);
